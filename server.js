82a9e5921effd624bb8fb4bd4fb836b8612ff257
zenaldo-oliveira <zenaldo_18@hotmail.com>
2024-09-28 15:46:41 -0400

    melhoria no beckend

--- server.js
+++ server.js
@@ -2,51 +2,43 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
 const app = express();
+app.use(express.json());
 
-app.use(express.json()); // Middleware para parsear JSON
-
-// Rota GET para listagem de usuários
 app.get("/usuarios", async (req, res) => {
   const users = await prisma.user.findMany();
+
   res.status(200).json(users);
 });
 
-// Rota POST para criar um novo usuário
 app.post("/usuarios", async (req, res) => {
-  try {
-    const user = await prisma.user.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-      },
-    });
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao criar usuário" });
-  }
+  const user = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+
+  res.status(201).json(user);
 });
 
-// Rota PUT para atualizar um usuário existente
 app.put("/usuarios/:id", async (req, res) => {
-  try {
-    const userId = parseInt(req.params.id); // Converte o ID para inteiro
-    const user = await prisma.user.update({
-      where: { id: userId },
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-      },
-    });
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao atualizar o usuário" });
-  }
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+    },
+  });
+
+  res.status(200).json(user);
 });
 
-// Rota DELETE para deletar um usuário
 app.delete("/usuarios/:id", async (req, res) => {
   await prisma.user.delete({
     where: {
@@ -54,10 +46,8 @@ app.delete("/usuarios/:id", async (req, res) => {
     },
   });
 
-  res.status(200).json({ messege: "Usuário deletado com sucesso" });
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
 });
 
-// Inicializando o servidor
-app.listen(3000, () => {
-  console.log("🚀server🚀online🚀");
-});
+app.listen(3000);
+console.log("🎈server🎈online🎈");
